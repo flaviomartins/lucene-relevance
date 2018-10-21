906b714a273ee3897f5da973d4a2fc76c28ab413
Flavio Martins <flaviomartins@acm.org>
2018-10-21 01:42:10 +0100

    Add LdpSimilarity

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 Contains implementations of good similarities for Lucene/Solr.
 - BM25Similarity: additional BM25L and BM25PLUS models to improve long document scores.
 - RobertsonSimilarity: tf-idf with Robertson's tf formula (BM25).
+- LdpSimilarity: td-idf with TF = 1+ln(1+ln(tf)) and pivoted document length normalization.
 - LtcSimilarity: tf-idf with sublinear tf (ltc). 
---
+++ src/main/java/org/novasearch/solr/search/similarities/LdpSimilarityFactory.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.novasearch.solr.search.similarities;
+
+import org.apache.lucene.search.similarities.Similarity;
+import org.apache.solr.common.params.SolrParams;
+import org.apache.solr.schema.SimilarityFactory;
+import org.novasearch.lucene.search.similarities.LdpSimilarity;
+
+/**
+ * Factory for {@link LdpSimilarity}
+ * <p>
+ * Parameters:
+ * <ul>
+ *   <li>b (float): Controls to what degree document length normalizes tf values.
+ *                  The default is <code>0.75</code>
+ *   <li>d (float): Controls lower-bound of term frequency normalization in Ldp.
+ *                  The default is <code>0.5</code>
+ * </ul>
+ * <p>
+ * Optional settings:
+ * <ul>
+ *   <li>discountOverlaps (bool): Sets
+ *       {@link LdpSimilarity#setDiscountOverlaps(boolean)}</li>
+ * </ul>
+ */
+public class LdpSimilarityFactory extends SimilarityFactory {
+  private boolean discountOverlaps;
+  private float b;
+  private float d;
+
+  @Override
+  public void init(SolrParams params) {
+    super.init(params);
+    discountOverlaps = params.getBool("discountOverlaps", true);
+    b = params.getFloat("b", 0.75f);
+    d = params.getFloat("d", 0.5f);
+  }
+
+  @Override
+  public Similarity getSimilarity() {
+    LdpSimilarity sim = new LdpSimilarity(b, d);
+    sim.setDiscountOverlaps(discountOverlaps);
+    return sim;
+  }
+}
